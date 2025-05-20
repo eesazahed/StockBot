558bd174e2deaecb9069e5b7a41fe4423f9746ca
Eesa Zahed <eesa.zahed@gmail.com>
2025-05-19 21:35:25 -0400

    added html page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-will complete later
+StockBot is a slack bot that allows you to fetch the most recent price of a stock or cryptocurrency.
+
+- `/stock SYMBOL`: will fetch the price of a stock based off the symbol
+- `/stock SYMBOL CURRENCY`: will fetch a stock based off the symbol and convert the currency from USD to chosen currency (example: CAD)
+
+- `/crypto SYMBOL`: will fetch the price of a cryptocurrency based off the symbol
+- `/crypto SYMBOL CURRENCY`: will fetch a cryptocurrency based off the symbol and convert the currency from USD to chosen currency (example: CAD)
+
+Hosted on [Nest](https://hackclub.app)
--- index.js
+++ index.js
@@ -13,78 +13,115 @@ const app = new App({
   port: Number(process.env.PORT) || 3000,
 });
 
-app.command("/stock", async ({ command, ack, respond }) => {
+app.command("/stock", async ({ command, ack, say }) => {
   await ack();
 
-  let symbol;
-  let toCurrency = "USD";
-
-  if (command.text.split(" ")[0]) {
-    symbol = command.text.split(" ")[0].trim().toUpperCase();
-  }
-
-  if (!symbol) {
-    return await respond(
-      "Please provide a valid stock symbol.\n\nExample: `/stock AAPL`"
-    );
-  }
-
-  if (command.text.split(" ")[1]) {
-    toCurrency = command.text.split(" ")[1].trim().toUpperCase();
-  }
-
-  const stockData = await fetchStockData({
-    symbol,
-    toCurrency,
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Fetching data for <@${command.user_id}>`,
+        },
+      },
+    ],
   });
 
+  let symbol = command.text.split(" ")[0]?.trim().toUpperCase();
+  let toCurrency = command.text.split(" ")[1]?.trim().toUpperCase() || "USD";
+
+  if (!symbol) {
+    return await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Please provide a valid stock symbol.\n\nExample: `/stock AAPL`",
+          },
+        },
+      ],
+    });
+  }
+
+  const stockData = await fetchStockData({ symbol, toCurrency });
+
   if (!stockData.success) {
-    return await respond(stockData.message);
+    return await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: stockData.message,
+          },
+        },
+      ],
+    });
   }
 
   const price = Number(stockData.price).toFixed(2);
   const change = Number(stockData.change);
   const percentChange = Number(stockData.percentChange);
   const isMarketOpen = Boolean(stockData.isMarketOpen);
-
   const isStockUp = change >= 0;
   const plusMinus = isStockUp ? "+" : "-";
   const currencySymbol = getCurrencySymbol(toCurrency);
 
-  return await respond(`
-    \n:tw_office: *Symbol:* \`${symbol}\`
-    \n:tw_dollar: *Price:* \`${currencySymbol}${price}\`
-    \n${
-      isStockUp ? ":tw_arrow_up:" : ":tw_arrow_down:"
-    } *Change:* \`${plusMinus}${currencySymbol}${Math.abs(change).toFixed(
-    2
-  )} (${Math.abs(percentChange).toFixed(2)}%)\`
-    \n${isMarketOpen ? ":tw_white_check_mark:" : ":tw_no_entry:"} Market is \`${
-    isMarketOpen ? "OPEN" : "CLOSED"
-  }\`
-    \n
-    \n<https://finance.yahoo.com/quote/${symbol}/|View Chart>
-  `);
+  return await say({
+    unfurl_links: false,
+    unfurl_media: false,
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `:tw_office: *Symbol:* \`${symbol}\`\n\n:tw_dollar: *Price:* \`${currencySymbol}${price}\`\n\n${
+            isStockUp ? ":tw_arrow_up:" : ":tw_arrow_down:"
+          } *Change:* \`${plusMinus}${currencySymbol}${Math.abs(change).toFixed(
+            2
+          )} (${Math.abs(percentChange).toFixed(2)}%)\`\n\n${
+            isMarketOpen ? ":tw_white_check_mark:" : ":tw_no_entry:"
+          } Market is \`${
+            isMarketOpen ? "OPEN" : "CLOSED"
+          }\`\n\n<https://finance.yahoo.com/quote/${symbol}/|View Chart>`,
+        },
+      },
+    ],
+  });
 });
 
-app.command("/crypto", async ({ command, ack, respond }) => {
+app.command("/crypto", async ({ command, ack, say }) => {
   await ack();
 
-  let symbol;
-  let toCurrency = "USD";
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Fetching data for <@${command.user_id}>`,
+        },
+      },
+    ],
+  });
 
-  if (command.text.split(" ")[0]) {
-    symbol = command.text.split(" ")[0].trim().toUpperCase();
-  }
+  let symbol = command.text.split(" ")[0]?.trim().toUpperCase();
+  let toCurrency = command.text.split(" ")[1]?.trim().toUpperCase() || "USD";
 
   if (!symbol) {
-    return await respond(
-      "Please provide a valid crypto symbol.\n\nExample: `/crypto BTC`"
-    );
-  }
-
-  if (command.text.split(" ")[1]) {
-    toCurrency = command.text.split(" ")[1].trim().toUpperCase();
+    return await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Please provide a valid crypto symbol.\n\nExample: `/crypto BTC`",
+          },
+        },
+      ],
+    });
   }
 
   const cryptoData = await fetchStockData({
@@ -92,37 +129,53 @@ app.command("/crypto", async ({ command, ack, respond }) => {
     toCurrency,
     crypto: "true",
   });
+
   if (!cryptoData.success) {
-    return await respond(cryptoData.message);
+    return await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: cryptoData.message,
+          },
+        },
+      ],
+    });
   }
 
   const price = Number(cryptoData.price).toFixed(2);
   const change = Number(cryptoData.change);
   const percentChange = Number(cryptoData.percentChange);
   const isMarketOpen = Boolean(cryptoData.isMarketOpen);
-
   const isCryptoUp = change >= 0;
   const plusMinus = isCryptoUp ? "+" : "-";
   const currencySymbol = getCurrencySymbol(toCurrency);
 
-  return await respond(`
-    \n:tw_office: *Symbol:* \`${symbol}\`
-    \n:tw_dollar: *Price:* \`${currencySymbol}${price}\`
-    \n${
-      isCryptoUp ? ":tw_arrow_up:" : ":tw_arrow_down:"
-    } *Change:* \`${plusMinus}${currencySymbol}${Math.abs(change).toFixed(
-    2
-  )} (${Math.abs(percentChange).toFixed(2)}%)\`
-    \n${isMarketOpen ? ":tw_white_check_mark:" : ":tw_no_entry:"} Market is \`${
-    isMarketOpen ? "OPEN" : "CLOSED"
-  }\`
-    \n
-    \n<https://finance.yahoo.com/quote/${symbol}-USD/|View Chart>
-  `);
+  return await say({
+    unfurl_links: false,
+    unfurl_media: false,
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `:tw_office: *Symbol:* \`${symbol}\`\n\n:tw_dollar: *Price:* \`${currencySymbol}${price}\`\n\n${
+            isCryptoUp ? ":tw_arrow_up:" : ":tw_arrow_down:"
+          } *Change:* \`${plusMinus}${currencySymbol}${Math.abs(change).toFixed(
+            2
+          )} (${Math.abs(percentChange).toFixed(2)}%)\`\n\n${
+            isMarketOpen ? ":tw_white_check_mark:" : ":tw_no_entry:"
+          } Market is \`${
+            isMarketOpen ? "OPEN" : "CLOSED"
+          }\`\n\n<https://finance.yahoo.com/quote/${symbol}-USD/|View Chart>`,
+        },
+      },
+    ],
+  });
 });
 
 (async () => {
   await app.start();
-
   app.logger.info("⚡️ Bolt app is running!");
 })();
